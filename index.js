f3738beb8834e7d561a03b1c8d44509b492f8286
trinhquang <qtrinh128@gmail.com>
2020-06-19 15:19:46 +0800

    add express

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ var app = express();
 app.set('view engine', 'ejs');
 // index page 
 app.get('/', function(req, res) {
-    res.render('pages/index');
-});
+    var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
+    res.render('pages/index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+});
 // about page 
 app.get('/about', function(req, res) {
     res.render('pages/about');
