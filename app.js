26fe975b47af4146aa7daea5e768783ee81a97a5
trinhquang <qtrinh128@gmail.com>
2020-06-19 14:43:20 +0800

    add express

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(req, res){
+  res.send('hello world');
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+app.listten(app.get('port'), ()=>{
+  console.log('app is runing');
 });
